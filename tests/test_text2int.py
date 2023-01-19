4120946ffa2c86e676fabc8db8dc1738382a5af1
Cetin CAKIR <cetincakirtr@gmail.com>
2023-01-19 12:53:44 +0300

    Update test file

--- tests/test_text2int.py
+++ tests/test_text2int.py
@@ -1,10 +1,11 @@
 import unittest
 
+import pandas as pd
 from fastapi.testclient import TestClient
 
 from app import app
 
-TEST_DATA_FILE = "data/test_data_text2int.csv"
+TEST_DATA_FILE = "data/master_test_text2int.csv"
 
 client = TestClient(app)
 
@@ -12,49 +13,39 @@ client = TestClient(app)
 class TestStringMethods(unittest.TestCase):
 
     def setUp(self):
+        """Creates a fastapi test client"""
         self.client = TestClient(app)
 
-    def test_1(self):
+    def get_response_text2int(self, text):
+        """Makes a post request to the endpoint"""
+        r = None
+        try:
+            r = self.client.post("/text2int", json={"content": text}) \
+                .json().get("message")
+        except:
+            pass
+        return r
+
+    def test_endpoint_text2int(self):
+        """Tests if endpoint is working"""
         response = self.client.post("/text2int",
                                     json={"content": "fourteen"}
                                     )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(str(response.json()["message"]), "14")
 
-    def test_2(self):
-        response = self.client.post("/text2int",
-                                    json={"content": "one thousand four hundred ninety two"}
-                                    )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(str(response.json()["message"]), "1492")
+    def test_acc_score_text2int(self):
+        """Calculates accuracy score for endpoint"""
+        df = pd.read_csv(TEST_DATA_FILE)
 
-    def test_3(self):
-        response = self.client.post("/text2int",
-                                    json={"content": "Fourteen Hundred Ninety-Two"}
-                                    )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(str(response.json()["message"]), "1492")
+        df["text2int"] = df["input"].apply(func=self.get_response_text2int)
+        df["score"] = df[["output", "text2int"]].apply(
+            lambda row: row[0] == row[1],
+            axis=1
+        )
+        df.to_csv("data/text2int_results.csv", index=False)
+        acc_score = df["score"].mean().__round__(2)
 
-    def test_4(self):
-        response = client.post("/text2int",
-                               json={"content": "forteen"}
-                               )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(str(response.json()["message"]), "14")
-
-    def test_5(self):
-        response = client.post("/text2int",
-                               json={"content": "seventeen-thousand and seventy two"}
-                               )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(str(response.json()["message"]), "17072")
-
-    def test_6(self):
-        response = client.post("/text2int",
-                               json={"content": "two hundred and nine"}
-                               )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(str(response.json()["message"]), "209")
+        self.assertGreaterEqual(acc_score, 0.8, f"Accuracy score: '{acc_score}'. Value is too low!")
 
 
 if __name__ == '__main__':
