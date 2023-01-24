b31816e2c9b4124bc42a5b65bf5d6e104a24ab86
Cetin Cakir <cetincakirtr@gmail.com>
2023-01-24 15:14:16 +0000

    Draft: Dev package

--- modules/sentiment.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-sentiment_obj = pipeline(task="sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
-
-
-def sentiment(text):
-    # Returns sentiment value
-    return sentiment_obj(text)
