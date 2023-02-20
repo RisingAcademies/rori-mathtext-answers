8ea1d70c11370660f86998d7855c811c1c05125d
sviddo <snisarvlad04@gmail.com>
2023-02-20 17:34:34 +0200

    add generate hint function

--- scripts/quiz/hints.py
+++ scripts/quiz/hints.py
@@ -1,34 +1,32 @@
-from random import random
-
-
 import random
 
-def generate_hint(question_numbers, right_answer, number_correct, number_incorrect, level, hints_used):
-    ordinal_number = question_numbers[1]
-    equation = right_answer - 2 * ordinal_number - 1
-    least_number = equation if equation > 0 else 0
-    seq_before_answer = " ".join(
-        [str(num) for num in range(right_answer - ordinal_number, least_number, -ordinal_number)][::-1]
-    )
-    seq_after_answer = " ".join(
-        [str(num) for num in range(right_answer + ordinal_number, right_answer + 2 * ordinal_number + 1, ordinal_number)]
-    )
+
+def generate_hint(question_nums, right_answer, right_answers, wrong_answers, level, hints_used):
+    ord_num = question_nums[1]  # ordinal number
+    equation = right_answer - 2 * ord_num - 1
+    min_num = equation if equation > 0 else 0
+    seq_before = " ".join(
+        [str(num) for num in range(right_answer - ord_num, min_num, -ord_num)][::-1]
+    )  # sequence before right answer
+    seq_after = " ".join(
+        [str(num) for num in range(right_answer + ord_num, right_answer + 2 * ord_num + 1, ord_num)]
+    )  # sequence after right answer
     hints = [
-        f"What number will fill the gap in a sequence {seq_before_answer} ... {seq_after_answer}?",
-        f"What number is {ordinal_number} in the account after {right_answer - ordinal_number}?",
-        f"What number is {ordinal_number} in the account before {right_answer + ordinal_number}?",
+        f"What number will fill the gap in a sequence {seq_before} ... {seq_after}?",
+        f"What number is {ord_num} in the account after {right_answer - ord_num}?",
+        f"What number is {ord_num} in the account before {right_answer + ord_num}?",
         f"What number is greater than {right_answer - 1} and less than {right_answer + 1}?"
     ]
-    random_hint = random.choice(hints)
+    rand_hint = random.choice(hints)
     hints_used += 1
 
-    data_to_return = {
-        "text": random_hint,
-        "question_numbers": question_numbers,
+    output = {
+        "text": rand_hint,
+        "question_numbers": question_nums,
         "right_answer": right_answer,
-        'number_correct': number_correct,
-        'number_incorrect': number_incorrect,
+        'number_correct': right_answers,
+        'number_incorrect': wrong_answers,
         'level': level,
         "hints_used": hints_used
     }
-    return data_to_return
+    return output
