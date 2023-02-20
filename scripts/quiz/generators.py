e267c9719fa0f7ff3602ea2c2c37db5d4fa6a2cb
sviddo <snisarvlad04@gmail.com>
2023-02-20 17:34:06 +0200

    update generators module

--- scripts/quiz/generators.py
+++ scripts/quiz/generators.py
@@ -1,36 +1,33 @@
-from .questions import generate_question_answer_pair
+from .questions import generate_question_data
 from .utils import get_next_level
 
 
-def start_interactive_math(successful_answers=0, wrong_answers=0, level="easy"):
+def start_interactive_math(right_answers=0, wrong_answers=0, level="easy"):
     if wrong_answers > 2:
         wrong_answers = 0
-        successful_answers = 0
+        right_answers = 0
         level = get_next_level(level, False)
-    elif successful_answers > 2:
-        successful_answers = 0
+    elif right_answers > 2:
+        right_answers = 0
         wrong_answers = 0
         level = get_next_level(level)
         
-    question_data = generate_question_answer_pair(level)
+    question_data = generate_question_data(level)
     question = question_data['question']
     right_answer = question_data['answer']
-    current_number = question_data['current_number']
-    ordinal_number = question_data['ordinal_number']
+    cur_num = question_data['current_number']
+    ord_num = question_data['ordinal_number']
+    times = question_data['times']
 
-    numbers_group = [current_number, ordinal_number]
-    if "times" in question_data:
-        times = question_data['times']
-        numbers_group.append(times)
-
-    data_to_return = {
+    numbers_group = [cur_num, ord_num, times]
+    output = {
         "text": question,
         "question_numbers": numbers_group,
         "right_answer": right_answer,
-        'number_correct': successful_answers,
+        'number_correct': right_answers,
         'number_incorrect': wrong_answers,
         'level': level,
         "hints_used": 0
     }
-    return data_to_return
+    return output
 
