494b5b574f2365ee898a63a6ebf7a54c23aa4471
Greg Thompson <thompsgj@yahoo.com>
2023-04-24 16:09:18 +0900

    Add comments to constants.py and run tests

--- mathtext_fastapi/constants.py
+++ mathtext_fastapi/constants.py
@@ -5,6 +5,7 @@ from supabase import create_client
 from dotenv import load_dotenv
 load_dotenv()
 
+# Intent recognition model file paths and names
 DATA_DIR = Path(__file__).parent.parent / "mathtext_fastapi" / "data"
 
 DEFAULT_MODEL_FILENAME = "intent_classification_model.joblib"
@@ -13,11 +14,12 @@ DEFAULT_LABELED_DATA = "labeled_data.csv"
 MODEL_PATH = DATA_DIR / DEFAULT_MODEL_FILENAME
 LABELED_DATA_PATH = DATA_DIR / DEFAULT_LABELED_DATA
 
+# Sentry monitoring link
 SENTRY_DSN = os.environ.get('SENTRY_DSN')
 
+# Supabase logging via sdk
 SUPABASE_URL = os.environ.get('SUPABASE_URL')
 SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
-
 SUPA = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
