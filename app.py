9ec271037b377709c361b2057fa40d2edd760341
Greg Thompson <thompsgj@yahoo.com>
2023-05-01 12:24:24 +0900

    Update nlu endpoint to log intent results correctly

--- app.py
+++ app.py
@@ -27,14 +27,14 @@ from pydantic import BaseModel
 
 log = getLogger(__name__)
 
-# sentry_sdk.init(
-#     dsn=SENTRY_DSN,
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
 
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     # We recommend adjusting this value in production,
-#     traces_sample_rate=1.0,
-# )
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
 
 app = FastAPI()
 
--- mathtext_fastapi/nlu.py
+++ mathtext_fastapi/nlu.py
@@ -202,9 +202,6 @@ def evaluate_message_with_nlu(message_data):
         log.error(f'Invalid student message: {message_data}')
         number_api_resp = TOKENS2INT_ERROR_INT
 
-    print("AFTER VALUEERROR")
-    print(number_api_resp)
-
     if number_api_resp == TOKENS2INT_ERROR_INT:
         # Run intent classification with logistic regression model
         nlu_response = predict_message_intent(message_text)
@@ -214,9 +211,6 @@ def evaluate_message_with_nlu(message_data):
             number_api_resp,
             0
         )
-
-    print("NLU RESPONSE")
-    print(nlu_response)
 
     prepare_message_data_for_logging(message_data, nlu_response)
     return nlu_response
