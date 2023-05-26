59ce3f058157c00edd72905ce760fc417d170d2f
Greg Thompson <thompsgj@yahoo.com>
2023-05-26 12:22:58 +0900

    Remove venv from build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,14 @@
-deactivate
-pip install --upgrade pytest-cov wheel pip virtualenv build
-rm -rf .venv
-python3.9 -m virtualenv --python 3.9 .venv
-source .venv/bin/activate || source .venv/scripts/activate
+if [ -f ".env" ] ; then
+    source .env
+fi
+
+if [ -f ".venv/bin/activate" ] ; then
+    source .venv/bin/activate
+fi
+
+pip install --upgrade pip poetry virtualenv wheel
+pip install --upgrade -e .
+
 # pip install --upgrade scikit-learn
 # pip install --upgrade transformers
 # pip install --upgrade pandas
-pip install --upgrade -e .
