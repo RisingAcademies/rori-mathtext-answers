6f7c9d527b9a8a8eac7df8a0027656049790f995
Greg Thompson <thompsgj@yahoo.com>
2023-05-26 12:19:29 +0900

    Update build.sh venv activation command

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@ deactivate
 pip install --upgrade pytest-cov wheel pip virtualenv build
 rm -rf .venv
 python3.9 -m virtualenv --python 3.9 .venv
-source .venv/bin/activate
+source .venv/bin/activate || source .venv/scripts/activate
 # pip install --upgrade scikit-learn
 # pip install --upgrade transformers
 # pip install --upgrade pandas
