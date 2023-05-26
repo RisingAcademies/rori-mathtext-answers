29a2c9ca925fbab47b2f5f5f5bef5960f5a3d090
Greg Thompson <thompsgj@yahoo.com>
2023-05-26 12:17:31 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 deactivate
-pip install virtualenv
+pip install --upgrade pytest-cov wheel pip virtualenv build
 rm -rf .venv
 python3.9 -m virtualenv --python 3.9 .venv
+source .venv/bin/activate
 # pip install --upgrade scikit-learn
 # pip install --upgrade transformers
 # pip install --upgrade pandas
