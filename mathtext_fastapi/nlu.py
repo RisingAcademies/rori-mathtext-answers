9a0e55124fb95ab46b879abc489061b876340ede
Greg Thompson <thompsgj@yahoo.com>
2023-06-23 10:44:18 +0900

    Update how normalized payload values are used

--- mathtext_fastapi/nlu.py
+++ mathtext_fastapi/nlu.py
@@ -176,14 +176,18 @@ async def evaluate_message_with_nlu(message_text, expected_answer):
         log.info(f'Starting evaluate message: {message_text}')
         nlu_response = {}
 
-        expected_answer = expected_answer.lower().replace(',','')
-        message_text = message_text.lower().replace(',','')
+        # NOTE: If the ',' normalization is removed from the stack
+        # ...this will need to be adjusted
+        expected_answer = expected_answer.replace(',', '')
+        message_text = message_text.replace(',', '')
 
         if len(message_text) < 50:
             # Check the student message for pre-defined keywords
-        
+            
             with sentry_sdk.start_span(description="Comparison Evaluation"):
-                if expected_answer.strip() == message_text.strip():
+                normalized_expected_answer = expected_answer.lower()
+                normalized_message_text = message_text.lower()
+                if normalized_expected_answer.strip() == normalized_message_text.strip():
                     nlu_response = format_nlu_response(
                         'comparison',
                         expected_answer,
