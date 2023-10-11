2784b057118692ced7b62a5393b43e50b1ff16bb
Greg Thompson <thompsgj@yahoo.com>
2023-10-11 15:59:40 +0900

    feature: add keyword and intent evaluation endpoint

--- mathtext_fastapi/v2_nlu.py
+++ mathtext_fastapi/v2_nlu.py
@@ -26,6 +26,18 @@ from mathtext_fastapi.response_formaters import build_single_event_nlu_response
 log = getLogger(__name__)
 
 
+async def run_keyword_and_intent_evaluations(text):
+    result = evaluate_for_exact_keyword_match_in_phrase(text, "", "")
+    if result and result != str(32202):
+        return build_single_event_nlu_response("keyword", result, 1.0)
+    result = predict_message_intent(text)
+    if result and result != str(32202):
+        return build_single_event_nlu_response(
+            "intent", result.get("data", ""), result.get("confidence", 0)
+        )
+    return ERROR_RESPONSE_DICT
+
+
 def evaluate_for_exact_match(normalized_student_message, normalized_expected_answer):
     """Compares the normalized student message and expected answers for a direct match
 
