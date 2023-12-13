6860f19898bd6f0aeae28aa41db612a40f0cf62b
Greg Thompson <thompsgj@yahoo.com>
2023-12-13 10:28:14 -0500

    style: run student_ability_model.py through black

--- mathtext_fastapi/django_logging/student_ability_model.py
+++ mathtext_fastapi/django_logging/student_ability_model.py
@@ -1,12 +1,19 @@
-
 def get_bkt_params(activity_session):
     try:
         params = activity_session.properties["bkt_params"]
-        return params["p_learn"], params["p_slip"], params["p_guess"], params["p_transit"]
+        return (
+            params["p_learn"],
+            params["p_slip"],
+            params["p_guess"],
+            params["p_transit"],
+        )
     except Exception:
         return None, None, None, None
 
-def calculate_lesson_mastery(curr_answer_type, prev_p_learn, p_slip, p_guess, p_transit):
+
+def calculate_lesson_mastery(
+    curr_answer_type, prev_p_learn, p_slip, p_guess, p_transit
+):
     """
     Calculates the probability of student mastering current lesson based on the most recent answer
     :param curr_answer_type:
@@ -19,11 +26,15 @@ def calculate_lesson_mastery(curr_answer_type, prev_p_learn, p_slip, p_guess, p_
     p_learn = prev_p_learn
 
     if curr_answer_type == "correct_answer":
-        cond_p = (prev_p_learn*(1-p_slip))/((prev_p_learn*(1-p_slip)) + (1-prev_p_learn)*p_guess)
+        cond_p = (prev_p_learn * (1 - p_slip)) / (
+            (prev_p_learn * (1 - p_slip)) + (1 - prev_p_learn) * p_guess
+        )
     elif curr_answer_type == "wrong_answer":
-        cond_p = (prev_p_learn*p_slip)/((prev_p_learn*p_slip) + (1-prev_p_learn)*(1-p_guess))
+        cond_p = (prev_p_learn * p_slip) / (
+            (prev_p_learn * p_slip) + (1 - prev_p_learn) * (1 - p_guess)
+        )
     else:
         return p_learn
 
-    p_learn = cond_p + (1-cond_p)*p_transit
+    p_learn = cond_p + (1 - cond_p) * p_transit
     return p_learn
